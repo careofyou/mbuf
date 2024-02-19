621bb46cb7eb9a78abeeb030b2ef5a2bf1211e5a
zxc <zxc@MacBook-Pro-Lapserdak.local>
2024-02-20 02:06:16 +0700

    last

--- components/Nav/NavLink.tsx
+++ components/Nav/NavLink.tsx
@@ -1,11 +1,12 @@
-import clsx from 'clsx';
-import Link, { LinkProps } from 'next/link';
-import { useRouter } from 'next/router';
+import clsx from "clsx"
+import Link, { LinkProps } from "next/link"
+import { useRouter } from "next/router"
 
 interface NavLinkProps extends LinkProps {
-  children: string;
-  isHeader?: boolean;
-  isMobileNavigation?: boolean;
+  className: string
+  children: string
+  isHeader?: boolean
+  isMobileNavigation?: boolean
 }
 
 const NavLink: React.FC<NavLinkProps> = ({
@@ -15,21 +16,29 @@ const NavLink: React.FC<NavLinkProps> = ({
   href,
   ...props
 }) => {
-  const router = useRouter();
+  const router = useRouter()
 
   const isRouteActive =
-    router.pathname.split('/')[1] === String(href).split('/')[1];
+    router.pathname.split("/")[1] === String(href).split("/")[1]
 
   return (
     <Link
       href={href}
-      className={clsx( 'motion-safe:transition-colors', isRouteActive ? 'font-semibold dark:text-blue-500' : 'capitalize inline-block text-3xl relative cursor-pointer transition-all duration-500 before:absolute before:-bottom-2 before:left-0 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100', isMobileNavigation ? 'text-2xl' : 'text-base', !isMobileNavigation && isHeader && 'rounded-lg p-2 hover:bg-gray-200 dark:hover:bg-blue-500',
+      className={clsx(
+        "motion-safe:transition-colors",
+        isRouteActive
+          ? "font-semibold dark:text-blue-500"
+          : "capitalize inline-block text-3xl relative cursor-pointer transition-all duration-500 before:absolute before:-bottom-2 before:left-0 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100",
+        isMobileNavigation ? "text-2xl" : "text-base",
+        !isMobileNavigation &&
+          isHeader &&
+          "rounded-lg p-2 hover:bg-gray-200 dark:hover:bg-blue-500"
       )}
       {...props}
     >
       {children}
     </Link>
-  );
-};
+  )
+}
 
-export default NavLink;
+export default NavLink
